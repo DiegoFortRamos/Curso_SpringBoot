5330822b163d9e174c73c9bbb2c7674c67aebe8f
DiegoFortRamos <fortunatodiegoramos@gmail.com>
2023-06-21 20:50:21 -0300

    Adicionando arquivos no projeto

---
+++ src/main/java/med/voll/api/controller/PacienteController.java
@@ -0,0 +1,12 @@
+package med.voll.api.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("pacientes")
+public class PacienteController {
+
+}
