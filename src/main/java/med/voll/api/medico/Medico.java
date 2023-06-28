da51056e6a2a014efc57eab7cdd9ad7b5f95a229
DiegoFortRamos <fortunatodiegoramos@gmail.com>
2023-06-27 22:16:40 -0300

    Adicionando novas funcionalidades

--- src/main/java/med/voll/api/medico/Medico.java
+++ src/main/java/med/voll/api/medico/Medico.java
@@ -6,6 +6,7 @@ import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import med.voll.api.endereco.DadosEndereco;
 import med.voll.api.endereco.Endereco;
 
 @Table(name = "medicos")
@@ -35,6 +36,19 @@ public class Medico {
         this.crm = dados.crm();
         this.especialidade = dados.especialidade();
         this.endereco = new Endereco(dados.endereco());
+    }
+
+    public void atualizaInformacoes(DadosAtualizaMedico dados){
+       if (dados.nome() != null){
+           this.nome = dados.nome();
+       }
+       if (dados.telefone() != null){
+           this.telefone = dados.telefone();
+       }
+       if (dados.endereco() != null){
+           this.endereco.atualizarInformacoes(dados.endereco());
+       }
+
     }
 
 
